4c88e1b6eb5b79325b432d7ef585985947c2b739
PastelEgg1101 <94875821+PastelEgg1101@users.noreply.github.com>
2024-09-25 05:27:16 -0400

    Added Main Menu and Playtest level
    Main menu has aniamtion where text bounces up and down but warning no transition scene
    Playtest level is also filled with gray boxes as these are the wind

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    void Update()
+    {
+        // Detect any button press on the joystick or keyboard
+        if (Input.anyKeyDown || AnyAnalogButtonPressed())
+        {
+            // Load the scene
+            LoadGame();
+            //Debug.Log("I EXIST");
+        }
+    }
+
+    // Method to detect joystick button press
+    bool AnyAnalogButtonPressed()
+    {
+        for (int i = 0; i < 20; i++) // https://discussions.unity.com/t/find-out-if-any-button-on-any-gamepad-has-been-pressed-and-which-one/65089
+        {
+            if (Input.GetKey("joystick button " + i))
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
---
+++ Assets/TansitionManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TansitionManager : MonoBehaviour
+{
+    public Animator transition;
+    public float delayTime = 1f; 
+
+}
