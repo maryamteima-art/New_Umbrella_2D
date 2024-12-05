c4f76a884f0fd271b82251878e3e2998b9ebc7e2
Maryam Teima <maryam.teima@gmail.com>
2024-12-05 18:56:52 -0500

    NewCutscenes scenes with proper filepaths

---
+++ Assets/Resources/Prefabs/Cutscenes/VideoPlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class VideoPlayerController : MonoBehaviour
+{
+    [SerializeField] string videoFileName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayVideo();
+    }
+
+    public void PlayVideo()
+    {
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+
+        if (videoPlayer)
+        {
+            string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
+            Debug.Log(videoPath);
+            videoPlayer.url = videoPath;
+            videoPlayer.Play();
+        }
+    }
+}
